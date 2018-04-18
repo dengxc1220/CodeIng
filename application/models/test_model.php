0c0a50268d312419680ff4dc564913015e578f9d
dengxc1220 <2831460536@qq.com>
2018-04-18 10:40:06 +0800

    CI
    start

---
+++ application/models/test_model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/4/18
+ * Time: 9:09
+ */
---
+++ application/views/test/test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/4/18
+ * Time: 9:26
+ */
+echo 'hello test.php';
